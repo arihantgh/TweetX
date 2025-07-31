838292a332491e947748d36310fc73615d036b71
arihant kothari <arihantkotharicodes@gmail.com>
2025-07-31 23:31:24 +0530

    added post tweet ui

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,54 +1,31 @@
-import { Button } from "@/components/ui/button"
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import CreateTweet from "./CreateTweet";
+import { Moon, Sun } from "lucide-react";
+import { useState } from "react";
 
 function Home() {
+  const [dark, setDark] = useState(true);
   return (
     <>
-        <Dialog>
-      <form>
-        <DialogTrigger asChild>
-          <Button variant="outline">Open Dialog</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you&apos;re
-              done.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4">
-            <div className="grid gap-3">
-              <Label htmlFor="name-1">Name</Label>
-              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="username-1">Username</Label>
-              <Input id="username-1" name="username" defaultValue="@peduarte" />
-            </div>
-          </div>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </form>
-    </Dialog>
+      <div className="flex items-center justify-between sticky top-0 z-50 p-5 bg-background">
+        <div>
+          <span className="font-medium text-lg">TweetX</span>
+        </div>
+        <div className="flex gap-2">
+          <Button variant="outline" onClick={()=>setDark(!dark)}>{dark ? <Sun /> : <Moon />}</Button>
+          <Dialog>
+            <form>
+              <DialogTrigger asChild>
+                <Button variant="default">Post</Button>
+              </DialogTrigger>
+              <CreateTweet />
+            </form>
+          </Dialog>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
